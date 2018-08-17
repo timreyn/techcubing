6479b7ac1a8a270ac358395f46869b12b9793575
Tim Reynolds <tim@cubingusa.org>
2018-08-17 07:29:37 -0400

    Limit to current comps, and display their names.

--- server/src/main/java/org/cubingusa/techcubing/handlers/CompetitionsHandler.java
+++ server/src/main/java/org/cubingusa/techcubing/handlers/CompetitionsHandler.java
@@ -2,6 +2,7 @@ package org.cubingusa.techcubing.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import org.cubingusa.techcubing.framework.ServerState;
@@ -14,10 +15,13 @@ public class CompetitionsHandler extends BaseHandler {
 
   @Override
   protected void handleImpl(HttpExchange t) throws IOException {
-    JSONArray response = (JSONArray) getWcaApi("/competitions?managed_by_me=true");
-    System.out.println(response.toString());
+    ZonedDateTime fromTime = ZonedDateTime.now().minusMonths(1);
+    JSONArray response = (JSONArray) getWcaApi(
+        "/competitions?managed_by_me=true&start=" + fromTime.toString());
 
     Map<String, Object> model = new HashMap<>();
+    model.put("competitions", response);
+
     writeResponse(model, "competitions.ftlh", t);
   }
 
