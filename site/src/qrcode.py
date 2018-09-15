7bed552f8708b17cc70780c39ed66ad1da29fe06
Tim Reynolds <tim@cubingusa.org>
2018-09-15 14:57:32 -0400

    Serve a (static) QR code from /qr.

--- site/src/qrcode.py
+++ site/src/qrcode.py
@@ -1,5 +1,10 @@
 import webapp2
 
+from src.jinja import JINJA_ENVIRONMENT
+
 class QRCodeHandler(webapp2.RequestHandler):
   def get(self):
-    self.response.write('Hello world!')
+    template = JINJA_ENVIRONMENT.get_template('qr.html')
+    self.response.write(template.render({
+      'qrcode': 'abc',
+    }))
